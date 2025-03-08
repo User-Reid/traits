187c34a4ed40d06062b82b9ce7cea1a08ae1ae35
Reid Herrera <reidherrerabusiness@gmail.com>
2025-03-08 13:30:40 -0600

    Understanding how to define a trait and writing rules/methods within the trait :). They are the rules of whatever type impliments the Accommidation trait :)

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,7 @@
-use std::ops::Add;
+trait Accommodation {
+    fn get_description(&self) -> String;
 
-fn add_two_numbers<T: Add<Output = T>>(a: T, b: T) -> T {
-    a + b
+    fn book(&mut self, name: &str, nights: u32);
 }
 
-fn main() {
-    let integer_sum = add_two_numbers(1, 2);
-    let float_sum = add_two_numbers(1.5, 2.4);
-    println!("{integer_sum} and {float_sum}");
-}
+fn main() {}
