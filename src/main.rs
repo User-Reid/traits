aa2e2085e09bf0d788c8603a3283c1cc0f88e5cb
Reid Herrera <reidherrerabusiness@gmail.com>
2025-03-10 19:52:07 -0500

    understanding how to impliment traits on types

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,33 @@
+use std::collections::HashMap;
+
 trait Accommodation {
     fn get_description(&self) -> String;
-
     fn book(&mut self, name: &str, nights: u32);
 }
 
-println!("Beggining tomorrow");
+#[derive(Debug)]
+struct Hotel {
+    name: String,
+    reservation: HashMap<String, u32>,
+}
+
+impl Hotel {
+    fn new(name: String) -> Self {
+        Self {
+            name,
+            reservation: HashMap::new(),
+        }
+    }
+}
+
+impl Accommodation for Hotel {
+    fn get_description(&self) -> String {
+        format!("{} is the pinnacle of luxery", self.name)
+    }
+
+    fn book(&mut self, name: &str, nights: u32) {
+        self.reservation.insert(name.to_string(), nights);
+    }
+}
 
 fn main() {}
