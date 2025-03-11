197b081cdd7863ced461d86429023b7b0f4e732c
Reid Herrera <reidherrerabusiness@gmail.com>
2025-03-10 19:52:49 -0500

    understanding how to impliment methods on triats into types and call them even though they are different types like vector and tuples

--- src/main.rs
+++ src/main.rs
@@ -30,4 +30,41 @@ impl Accommodation for Hotel {
     }
 }
 
-fn main() {}
+#[derive(Debug)]
+struct AirBnB {
+    host: String,
+    guests: Vec<(String, u32)>,
+}
+
+impl AirBnB {
+    fn new(host: String, guests: u32) -> Self {
+        Self {
+            host,
+            guests: vec![],
+        }
+    }
+}
+
+impl Accommodation for AirBnB {
+    fn get_description(&self) -> String {
+        format!("Please enjoy {}'s apartment!", self.host)
+    }
+
+    fn book(&mut self, name: &str, nights: u32) {
+        self.guests.push((name.to_string(), nights))
+    }
+}
+
+fn main() {
+    let mut hotel: Hotel = Hotel::new(String::from("The Luxe"));
+    println!("{:#?}", hotel.get_description());
+
+    let mut airbnb: AirBnB = AirBnB::new(String::from("Peter"), 45);
+    println!("{:?}", airbnb.get_description());
+
+    hotel.book("Robert", 23);
+    airbnb.book("Taco", 74);
+
+    println!("{hotel:?}");
+    println!("{airbnb:?}");
+}
