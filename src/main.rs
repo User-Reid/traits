7af229535609ce78cb921e57085b38a728d00c0b
Reid Herrera <reidherrerabusiness@gmail.com>
2025-03-17 23:30:29 -0500

    made a quick adjust

--- src/main.rs
+++ src/main.rs
@@ -72,4 +72,6 @@ fn main() {
     let mut airbnb: AirBnB = AirBnB::new(String::from("Peter"), 45);
     mix_and_match(&mut hotel, &mut airbnb, "Tacobell");
     println!("{:#?}, {:#?}", hotel, airbnb);
+
+    println!("quick adjustment.")
 }
